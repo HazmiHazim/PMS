f088899961508c8af77894afe8fe6d8f6bc0e6f3
HazmiHazim <hazimnetto01@gmail.com>
2023-12-11 21:41:00 +0800

    Fix Height of Sidebar

--- resources/views/Admin/main.blade.php
+++ resources/views/Admin/main.blade.php
@@ -24,7 +24,7 @@
 
     <div class="flex">
         <aside id="separator-sidebar"
-            class="relative top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+            class="relative top-0 left-0 z-40 w-64 h-auto transition-transform -translate-x-full sm:translate-x-0"
             aria-label="Sidebar">
             <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
                 <ul class="space-y-2 font-medium">
