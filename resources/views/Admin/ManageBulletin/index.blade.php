4d469597971d50cc4fbae573f6c48d184be29610
HazmiHazim <hazimnetto01@gmail.com>
2023-12-27 13:04:38 +0800

    Fix Delete Bug that Always Delete First Record

--- resources/views/Admin/ManageBulletin/index.blade.php
+++ resources/views/Admin/ManageBulletin/index.blade.php
@@ -59,20 +59,20 @@
                                                 @endif
                                                 @if (Auth()->user()->role === 'coordinator')
                                                     <td class="whitespace-nowrap py-4">
-                                                        <button data-modal-target="delete-modal"
-                                                            data-modal-toggle="delete-modal"
+                                                        <button data-modal-target="delete-modal-{{ $news->bulletin_id }}"
+                                                            data-modal-toggle="delete-modal-{{ $news->bulletin_id }}"
                                                             class="font-medium text-red-600 dark:text-red-500 hover:underline"
                                                             type="button">
                                                             Delete
                                                         </button>
-                                                        <div id="delete-modal" tabindex="-1"
+                                                        <div id="delete-modal-{{ $news->bulletin_id }}" tabindex="-1"
                                                             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                                             <div class="relative p-4 w-full max-w-md max-h-full">
                                                                 <div
                                                                     class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                                                                     <button type="button"
                                                                         class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                                        data-modal-hide="delete-modal">
+                                                                        data-modal-hide="delete-modal-{{ $news->bulletin_id }}">
                                                                         <svg class="w-3 h-3" aria-hidden="true"
                                                                             xmlns="http://www.w3.org/2000/svg"
                                                                             fill="none" viewBox="0 0 14 14">
@@ -104,7 +104,8 @@
                                                                             @method('DELETE')
 
                                                                             @csrf
-                                                                            <button data-modal-hide="delete-modal"
+                                                                            <button
+                                                                                data-modal-hide="delete-modal-{{ $news->bulletin_id }}"
                                                                                 type="submit"
                                                                                 class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
                                                                                 Yes, I'm sure
@@ -112,7 +113,8 @@
                                                                             <input type="hidden" name="_token"
                                                                                 value="{{ csrf_token() }}">
                                                                         </form>
-                                                                        <button data-modal-hide="delete-modal"
+                                                                        <button
+                                                                            data-modal-hide="delete-modal-{{ $news->bulletin_id }}"
                                                                             type="button"
                                                                             class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
                                                                             cancel</button>
